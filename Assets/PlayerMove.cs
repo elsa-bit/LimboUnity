beb4c3b54e77de2e7537dfdfc7bec2c8107bd5a9
Elsa Firmin <elsafirmin@hotmail.fr>
2024-01-17 20:29:58 +0100

    Animation Player

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public float speed = 10.4f;
+    public Animator animator;
 
     void Update()
     {
@@ -11,27 +12,32 @@ public class PlayerMove : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
         {
             position.y += speed * Time.deltaTime;
+            animator.SetFloat("W", Input.GetKey(KeyCode.W) ? 1f : 0f);
             Debug.Log("W");
         }
 
         if (Input.GetKey(KeyCode.S))
         {
             position.y -= speed * Time.deltaTime;
+            animator.SetFloat("S", Input.GetKey(KeyCode.S) ? 1f : 0f);
             Debug.Log("S");
         }
 
         if (Input.GetKey(KeyCode.A))
         {
             position.x -= speed * Time.deltaTime;
+            animator.SetFloat("A", Input.GetKey(KeyCode.A) ? 1f : 0f);
             Debug.Log("A");
         }
 
         if (Input.GetKey(KeyCode.D))
         {
             position.x += speed * Time.deltaTime;
+            animator.SetFloat("D", Input.GetKey(KeyCode.D) ? 1f : 0f);
             Debug.Log("D");
         }
 
         transform.position = position;
+        animator.SetFloat("Speed", speed);
     }
 }
