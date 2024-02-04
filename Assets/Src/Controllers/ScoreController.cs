a27cdf34f83ff5dcb3bbc0ec75fd0d6a0292699a
Ancelotow <ancelotow@gmail.com>
2024-02-04 23:39:23 +0100

    Score level ✨

--- Assets/Src/Controllers/ScoreController.cs
+++ Assets/Src/Controllers/ScoreController.cs
@@ -1,5 +1,6 @@
 using System;
 using Src;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,7 +9,7 @@ public class ScoreController : MonoBehaviour
     void Start()
     {
         var timers = GameManager.Instance.GetTimersByLevel();
-        var textComponent = GetComponent<Text>();
+        var textComponent = GetComponent<TextMeshProUGUI>();
         var textScore = String.Empty;
         textScore += $"Level 1 : {timers[GameLevel.One].ToString()}\n";
         textScore += $"Level 2 : {timers[GameLevel.Two].ToString()}\n";
